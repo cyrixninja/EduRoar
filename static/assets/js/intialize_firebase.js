a94a7cea79ce60ef91417bd21d5464bba288212d
cyrixninja <cyrixninja@outlook.com>
2024-02-20 13:33:35 +0530

    Fix issues

--- static/assets/js/intialize_firebase.js
+++ static/assets/js/intialize_firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCxHYxcENbPNzwzoBOlQa48orv09s2BXpM",
-    authDomain: "solutionchallenge24-414313.firebaseapp.com",
-    databaseURL: "https://solutionchallenge24-414313-default-rtdb.firebaseio.com",
-    projectId: "solutionchallenge24-414313",
-    storageBucket: "solutionchallenge24-414313.appspot.com",
-    messagingSenderId: "1019418506703",
-    appId: "1:1019418506703:web:dc827b97e8b68da77c4eba"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
